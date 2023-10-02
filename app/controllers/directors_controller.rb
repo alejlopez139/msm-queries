d453a73247d6ba776c98468570ae05b63bad620c
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-10-02 20:03:40 +0000

    finished directors#youngest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -14,4 +14,13 @@ class DirectorsController < ApplicationController
     render ({:template => "director_templates/details"})
   end
 
+  def youngest
+    
+    render ({:template => "director_templates/youngest"})
+  end
+
+  def oldest
+    
+    render({:template => "director_templates/oldest"})
+  end
 end
